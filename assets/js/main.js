c3fbab94a6618817d2ab2599f1f037f2dc224ddb
tenapato <tenapatricio@gmail.com>
2021-09-13 10:52:14 -0500

    Added light/dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,9 +120,43 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+
+    if(this.scrollY >= 80){
+        nav.classList.add('scroll-header'); 
+    }else{
+        nav.classList.remove('scroll-header')
+    }
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+}
+
+window.addEventListener('scroll', scrollHeader)
+
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' :'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () =>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
